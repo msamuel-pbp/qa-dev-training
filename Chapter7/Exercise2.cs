14e2371e9c728289c419319b74ff69b68b1803b1
Mike Samuel <msamuel@paybyphone.com>
2014-03-24 19:11:18 -0700

    added chapter 7 exercises

---
+++ Chapter7/Exercise2.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Chapter7
+{
+    [TestFixture]
+    public class Exercise2
+    {
+        public class Math
+        {
+            public static double Add(double One, double Two)
+            {
+                return (One + Two);
+            }
+            public static double Subtract(double One, double Two)
+            {
+                return (One - Two);
+            }
+            public static double Multiply(double One, double Two)
+            {
+                return (One * Two);
+            }
+            public static double Divide(double One, double Two)
+            {
+                return (One / Two);
+            }
+        }
+
+        [Test]
+        public void MathTest()
+        {
+            Assert.AreEqual(15d, Math.Add(10, 5), "Result should be 15");
+            Assert.AreEqual(5d, Math.Subtract(10, 5), "Result should be 5");
+            Assert.AreEqual(50d, Math.Multiply(10, 5), "Result should be 50");
+            Assert.AreEqual(2d, Math.Divide(10, 5), "Result should be 2");
+        }
+    }
+}
---
+++ Chapter7/Exercise4.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter7
+{
+    [TestFixture]
+    public class Exercise4
+    {
+        public class Point
+        {
+            public int x;
+            public int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void PrintCoord()
+            {
+                Console.WriteLine("x is {0} and y is {1}", x, y);
+            }
+        }
+
+        public class Square
+        {
+            // A  B
+            // C  D
+            private Point A, B, C, D;
+
+            public Square(Point A, int length)
+            {
+                this.A = A;
+                B = new Point(A.x + length, A.y);
+                C = new Point(A.x, A.y - length);
+                D = new Point(B.x, C.y);
+            }
+
+            public void Print()
+            {
+                A.PrintCoord();
+                B.PrintCoord();
+                C.PrintCoord();
+                D.PrintCoord();
+            }
+        }
+
+        [Test]
+        public void squareTest()
+        {
+            int x = 5;
+            int y = 5;
+            int length = 3;
+
+            Point start = new Point(x,y);
+            Square square = new Square(start, length);
+
+            Console.WriteLine("Start at {0},{1} with a length of {2}",x,y,length);
+            square.Print();
+        }
+    }
+}
