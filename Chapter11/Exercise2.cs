afd8ac619eecb04ab8bf8d6a8563df244e5853a1
Mike Samuel <msamuel@paybyphone.com>
2014-08-09 14:54:06 -0700

    added chapter 11 exercises

---
+++ Chapter11/Exercise2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter11
+{
+    [TestFixture]
+    class Exercise2
+    {
+        public class Telephone
+        {
+            protected string phonetype;
+
+            public virtual void Ring()
+            {
+                Console.WriteLine("Ringing the " + phonetype);
+            }
+        }
+
+        class PublicBellPhone : Telephone
+        {
+            public PublicBellPhone()
+            {
+                phonetype = "Analog";
+            }
+        }
+
+        public class iPhone : Telephone
+        {
+            public override void Ring()
+            {
+                Console.WriteLine("Siri says hi!");
+            }
+        }
+
+        [Test]
+        public void ringthyphone()
+        {
+            var rotary = new PublicBellPhone();
+            rotary.Ring();
+            
+            var digital = new iPhone();
+            digital.Ring();
+        }
+    }
+}
---
+++ Chapter11/Exercise3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter11
+{
+    [TestFixture]
+    class Exercise3
+    {
+        public abstract class Telephone
+        {
+            protected string phonetype;
+
+            public abstract void Ring();
+        }
+
+        public class DigitalPhone : Telephone
+        {
+            public DigitalPhone()
+            {
+                phonetype = "digital";
+            }
+
+            public override void Ring()
+            {
+                Console.WriteLine("I am " + phonetype);
+            }
+        }
+
+        public class TalkingPhone : Telephone
+        {
+            public TalkingPhone()
+            {
+                phonetype = "talkingphone1000";
+            }
+
+            public override void Ring()
+            {
+                Console.WriteLine("I am " + phonetype);
+            }
+        }
+
+        [Test]
+        public void abstractedphones()
+        {
+            var d = new DigitalPhone();
+            var t = new TalkingPhone();
+
+            d.Ring();
+            t.Ring();
+        }
+    }
+}
