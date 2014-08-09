0767ba2dcce5385000475fc40aabf591c3fe69c6
Mike Samuel <msamuel@paybyphone.com>
2014-08-09 15:21:30 -0700

    added chapter 12 exercises

---
+++ Chapter12/Exercise3.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter12
+{
+    class Exercise3
+    {
+        public class Invoice
+        {
+            public string Vendor { get; set; }
+            public double Amount { get; set; }
+
+            public Invoice(string v = "", double a = 0.0)
+            {
+                Vendor = v;
+                Amount = a;
+            }
+
+            public static Invoice operator+ (Invoice lhs, Invoice rhs)
+            {
+                if (lhs == rhs)
+                {
+                    return new Invoice(lhs.Vendor, lhs.Amount + rhs.Amount);
+                }
+
+                return new Invoice();
+            }
+
+            public override bool Equals(object obj)
+            {
+                if ( !(obj is Invoice) )
+                {
+                    return false;
+                }
+
+                return this == (Invoice) obj;
+            }
+
+            public static bool operator== (Invoice lhs, Invoice rhs)
+            {
+                if (lhs.Vendor.Equals(rhs.Vendor) && (lhs.Amount == rhs.Amount))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public static bool operator!=(Invoice lhs, Invoice rhs)
+            {
+                return !(lhs == rhs);
+            }
+
+            public static bool operator< (Invoice lhs, Invoice rhs)
+            {
+                return (lhs.Amount < rhs.Amount);
+            }
+
+            public static bool operator> (Invoice lhs, Invoice rhs)
+            {
+                if (!(lhs < rhs) && (lhs.Amount != rhs.Amount))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public static bool operator<= (Invoice lhs, Invoice rhs)
+            {
+                return !(lhs > rhs);
+            }
+
+            public static bool operator>= (Invoice lhs, Invoice rhs)
+            {
+                return !(lhs < rhs);
+            }
+
+            public override string ToString()
+            {
+                return ("Vendor: " + Vendor + "\r\nAmount: " + Amount);
+            }
+        }
+
+        [Test]
+        public void blankInvoice()
+        {
+            var i = new Invoice();
+            Console.WriteLine(i.ToString());
+            Assert.IsNullOrEmpty(i.Vendor);
+            Assert.True(i.Amount == 0.0);
+        }
+
+        [Test]
+        public void setInvoice()
+        {
+            var i = new Invoice("UBC", 5d);
+            Console.WriteLine(i);
+            Assert.That(i.Vendor.Equals("UBC"));
+            Assert.True(i.Amount == 5d);
+        }
+
+        [Test]
+        public void checkEqual()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("UBC", 5d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 == invoice2);
+        }
+
+        [Test]
+        public void checkNotEqual()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("SFMTA", 3d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 != invoice2);
+        }
+
+        [Test]
+        public void checkNotEqualVendor()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("SFMTA", 5d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 != invoice2);
+        }
+
+        [Test]
+        public void checkNotEqualAmount()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("UBC", 3d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 != invoice2);
+        }
+
+        [Test]
+        public void checkLessThan()
+        {
+            var invoice1 = new Invoice("UBC", 3d);
+            var invoice2 = new Invoice("SFMTA", 5d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 < invoice2);
+        }
+
+        [Test]
+        public void checkLessThanOrEqual()
+        {
+            var invoice1 = new Invoice("UBC", 3d);
+            var invoice2 = new Invoice("SFMTA", 5d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 <= invoice2);
+
+            invoice1.Amount = 5d;
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 <= invoice2);
+        }
+
+        [Test]
+        public void checkGreaterThan()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("SFMTA", 3d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 >= invoice2);
+        }
+
+        [Test]
+        public void checkGreaterThanOrEqual()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("SFMTA", 3d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 >= invoice2);
+
+            invoice2.Amount = 5d;
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            Assert.True(invoice1 >= invoice2);
+        }
+
+        [Test]
+        public void addSameInvoice()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("UBC", 5d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            var i = invoice1 + invoice2;
+            Console.WriteLine("Invoice:\n" + i);
+            Assert.That(i.Vendor.Equals("UBC"));
+            Assert.True(i.Amount == 10d);
+        }
+
+        [Test]
+        public void addSameVendorOnly()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("UBC", 10d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            var i = invoice1 + invoice2;
+            Console.WriteLine("Invoice:\n" + i);
+            Assert.IsNullOrEmpty(i.Vendor);
+            Assert.True(i.Amount == 0.0);
+        }
+
+        [Test]
+        public void addSameAmountOnly()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("SFMTA", 5d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            var i = invoice1 + invoice2;
+            Console.WriteLine("Invoice:\n" + i);
+            Assert.IsNullOrEmpty(i.Vendor);
+            Assert.True(i.Amount == 0.0);
+        }
+
+        [Test]
+        public void addDiffInvoice()
+        {
+            var invoice1 = new Invoice("UBC", 5d);
+            var invoice2 = new Invoice("SFMTA", 3d);
+            Console.WriteLine("Invoice1:\n" + invoice1);
+            Console.WriteLine("Invoice2:\n" + invoice2);
+
+            var i = invoice1 + invoice2;
+            Console.WriteLine("Invoice:\n" + i);
+            Assert.IsNullOrEmpty(i.Vendor);
+            Assert.True(i.Amount == 0.0);
+        }
+    }
+}
---
+++ Chapter12/Exercise4.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter12
+{
+    [TestFixture]
+    class Exercise4
+    {
+        private const double FeetInMeters = 3.28084;
+
+        public class Foot
+        {
+            public double Length { get; set; }
+
+            public Foot(double feet) { Length = feet; }
+            public Foot(Meter m)     { Length = m.Length * FeetInMeters; }
+
+            public static explicit operator Meter(Foot f)
+            {
+                return new Meter(f);
+            }
+
+            public override string ToString()
+            {
+                return "Length in Feet is " + Length; ;
+            }
+        }
+
+        public class Meter
+        {
+            public double Length { get; set; }
+
+            public Meter(double meter) { Length = meter; }
+            public Meter(Foot f)       { Length = f.Length / FeetInMeters; }
+
+            public static explicit operator Foot(Meter m)
+            {
+                return new Foot(m);
+            }
+
+            public override string ToString()
+            {
+                return "Length in Meters is " + Length;
+            }
+        }
+
+        [Test]
+        public void NewFoot()
+        {
+            var f = new Foot(5);
+         
+            Console.WriteLine(f);
+            Assert.True(f.Length == 5d);
+        }
+
+        [Test]
+        public void NewMeter()
+        {
+            var m = new Meter(5);
+
+            Console.WriteLine(m);
+            Assert.True(m.Length == 5d);
+        }
+
+        [Test]
+        public void FootToMeter()
+        {
+            var f = new Foot(5);
+            var m = (Meter) f;
+
+            Console.WriteLine(f);
+            Console.WriteLine(m);
+            Assert.True(m.Length == f.Length / FeetInMeters);
+        }
+
+        [Test]
+        public void MeterToFoot()
+        {
+            var m = new Meter(5);
+            var f = (Foot) m;
+
+            Console.WriteLine(f);
+            Console.WriteLine(m);
+            Assert.True(f.Length == m.Length * FeetInMeters);            
+        }
+    }
+}
