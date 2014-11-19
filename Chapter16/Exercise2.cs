ec6ed66936cb4210fa11ed66c451f15c2649e4c5
Mike Samuel <msamuel@paybyphone.com>
2014-11-18 17:31:28 -0800

    added chapter16 exercises

---
+++ Chapter16/Exercise2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Chapter16 {
+    [TestFixture]
+    public class Exercise2 {
+        private int[] iarray = { 1, 12, 19 };
+
+        public void GetIndex(object value) {
+            try {
+                Console.WriteLine("Accessing index {0}", value);
+                if (!(value is int)) {
+                    throw new FormatException();
+                }
+                var intval = (int)value;
+
+                if (intval < 0 || intval > 2) {
+                    throw new IndexOutOfRangeException();
+                }
+
+                Console.WriteLine("Index {0} : {1}", intval, iarray[intval]);
+            } catch (FormatException) {
+                Console.WriteLine("Index can only be int");
+            } catch (IndexOutOfRangeException) {
+                Console.WriteLine("Index range is only between 0-2");
+            }
+        }
+
+        [Test]
+        public void TestArray() {
+            GetIndex(2);
+            GetIndex(-1);
+            GetIndex('a');
+        }
+    }
+}
---
+++ Chapter16/Exercise3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Chapter16 {
+    [TestFixture]
+    public class Exercise3 {
+        private List<Cat> catlist = new List<Cat>();
+
+        public class Cat {
+            public int Age { get; set; }
+        }
+
+        public void AddCat(object value) {
+            try {
+                Console.WriteLine("Processing cat that is {0} years old", value);
+
+                if (!(value is int)) {
+                    throw new FormatException();
+                }
+
+                var age = (int)value;
+                if ((age <= 0) || (age > 25)) {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                Cat c = new Cat();
+                c.Age = age;
+                catlist.Add(c);
+            } catch (ArgumentOutOfRangeException) {
+                Console.WriteLine("Cats can only be within 1-25 years");
+            } catch (Exception e) {
+                Console.WriteLine("Unknown error happened: {0}", e.Message);
+            } finally {
+                Console.WriteLine("Cat consumed");
+                catlist.Clear();
+            }
+        }
+
+        [Test]
+        public void TestAge() {
+            AddCat(1);
+            AddCat(6);
+            AddCat(11);
+            AddCat('a');
+            AddCat(-1);
+            AddCat(0);
+            AddCat(26);
+        }
+    }
+}
