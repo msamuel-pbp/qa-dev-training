6fe57810a713965960c0ba4d78c4856838b5a1d8
Mike Samuel <msamuel@paybyphone.com>
2015-02-06 19:33:41 -0800

    added Chapter 19 samples

---
+++ Chapter19/Rectangle.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Chapter19 {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
