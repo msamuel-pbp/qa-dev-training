a39b63ee8d7a46daa1e70df497469404f5c1f1ed
Mike Samuel <msamuel@paybyphone.com>
2015-02-09 15:25:16 -0800

    added chapter 19 exercises

---
+++ Chapter19/Exercise3.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chapter19 {
+    /// <summary>
+    /// Interaction logic for Exercise3.xaml
+    /// </summary>
+    public partial class Exercise3 {
+        public Exercise3() {
+            InitializeComponent();
+        }
+    }
+}
