1b68d7f3108aaf26eeedd231f18fbc5caf3f0466
Mike Samuel <msamuel@paybyphone.com>
2015-02-16 21:08:36 -0800

    added chapter 20 samples and exercise 1

---
+++ Chapter20/Exercise1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter20 {
+    public class Exercise1 {
+        public class Box {
+            public int Length { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public void DisplayBox() {
+                Console.WriteLine("{0}x{1}x{2}", Length, Width, Height);
+            }
+        }
+        public class Boxes : List<Box> { }
+
+        public static void Main(string[] args) {
+            var storage = new Boxes();
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < 5; i++) {
+                var ups = new Box();
+                ups.Length = rand.Next(1, 5);
+                ups.Width = rand.Next(1, 5);
+                ups.Height = rand.Next(1, 5);
+                storage.Add(ups);
+            }
+
+            var query =
+                from b in storage
+                where b.Length > 3 && b.Width > 3
+                select b;
+
+            Console.WriteLine("Total boxes");
+            foreach (var b in storage) {
+                b.DisplayBox();
+            }
+            Console.WriteLine("Found boxes");
+            foreach (var b in query) {
+                b.DisplayBox();
+            }
+        }
+    }
+}
---
+++ Chapter20/Sample2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace Chapter20 {
+    public class Sample2 {
+        public static void Maint2(string[] args) {
+            var db = new SampleDatabaseDataContext();
+
+            var empQuery =
+                from emp in db.Employees
+                select emp;
+            Console.WriteLine("Employee List");
+            foreach (var e in empQuery) {
+                Console.WriteLine("ID:{0},Name:{1} {2}",e.EmployeeID,e.FirstName,e.LastName);
+            }
+
+            var orderQuery =
+                from order in db.Order_Details
+                where order.OrderID >= 10250 && order.OrderID <= 10255
+                select order;
+            Console.WriteLine("Product orders between 10250 and 10255");
+            foreach (var o in orderQuery) {
+                Console.WriteLine("ID:{0}\tQty:{1}\tProduct:{2}", o.OrderID, o.Quantity, o.Product);
+            }
+
+            db.Dispose();
+        }            
+    }
+}
