ea62d57be8ff361a6dfe1bceb1726d0424882a06
Mike Samuel <msamuel@paybyphone.com>
2015-02-21 19:34:46 -0800

    added exercises for chapter 20

---
+++ Chapter20/Exercise2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Chapter20 {
+    public class Exercise2 {
+        public void Run() {
+            var connection = ConfigurationManager.ConnectionStrings["SampleDatabaseConnectionString"].ConnectionString;
+            var command = @"select ProductID,ProductName from Products p
+                            where p.UnitsInStock < 10";
+            var dataAdapter = new SqlDataAdapter(command, connection);
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+
+            var dataTable = dataSet.Tables[0];
+            foreach (DataRow row in dataTable.Rows) {
+                Console.WriteLine("ID: {0},Name: {1}", row["ProductID"], row["ProductName"]);
+            }
+        }
+    }
+}
---
+++ Chapter20/Exercise3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Chapter20 {
+    public class Exercise3 {
+        public void Run() {
+            var connection = ConfigurationManager.ConnectionStrings["SampleDatabaseConnectionString"].ConnectionString;
+            var command = @"select distinct e.LastName,e.FirstName 
+                            from Employees e
+                            left join EmployeeTerritories et on et.EmployeeID = e.EmployeeID
+                            left join Territories t on t.TerritoryID = et.TerritoryID
+                            where t.RegionID = 1";
+            var dataAdapter = new SqlDataAdapter(command, connection);
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+
+            var dataTable = dataSet.Tables[0];
+            foreach (DataRow row in dataTable.Rows) {
+                Console.WriteLine("LastName: {0},FirstName: {1}", row["LastName"], row["FirstName"]);
+            }
+        }
+    }
+}
